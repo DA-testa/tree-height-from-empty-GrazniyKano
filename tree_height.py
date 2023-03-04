2857948cee11b4c9c351de9ad793fc2a9352f581
GrazniyKano <77270910+GrazniyKano@users.noreply.github.com>
2023-03-04 13:35:13 +0000

    Vladislavs_Vascenko_221RDC057

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,23 @@
-# python3
+def izveidot_koku(n, vecaki):
+    koks = [[] for i in range(n)]
+    for i in range(n):
+        if vecaki[i] == -1:
+            sakne = i
+        else:
+            koks[vecaki[i]].append(i)
+    return koks, sakne
 
-import sys
-import threading
-import numpy
+def aprekinat_augstums(koks, virsotne):
+    if not koks[virsotne]:
+        return 1
+    else:
+        limeni = [aprekinat_augstums(koks, berns) for berns in koks[virsotne]]
+        return max(limeni) + 1
 
+n = int(input())
+vecaki = list(map(int, input().split()))
 
-def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+koks, sakne = izveidot_koku(n, vecaki)
+augstums = aprekinat_augstums(koks, sakne)
 
-
-def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+print(augstums)
